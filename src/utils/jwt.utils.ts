72bc39eda62899ee8339d249582a074b20663d35
Eslam Amin <eslam.amin.610@gmail.com>
2026-02-05 20:29:25 +0200

    added refresh token, and access token function

--- src/utils/jwt.utils.ts
+++ src/utils/jwt.utils.ts
@@ -8,6 +8,26 @@ export const signToken = (payload: object): string => {
   });
 };
 
+export const signAccessToken = (payload: object): string => {
+  return jwt.sign({ ...payload, type: "access" }, config.jwt.access.secret, {
+    expiresIn: config.jwt.access.expiresIn as StringValue
+  });
+};
+
+export const signRefreshToken = (payload: object): string => {
+  return jwt.sign({ ...payload, type: "refresh" }, config.jwt.access.secret, {
+    expiresIn: config.jwt.refresh.expiresIn as StringValue
+  });
+};
+
+export const verifyAccessToken = (token: string): any => {
+  return jwt.verify(token, config.jwt.access.secret);
+};
+
+export const verifyRefreshToken = (token: string): any => {
+  return jwt.verify(token, config.jwt.refresh.secret);
+};
+
 export const verifyToken = (token: string): any => {
   return jwt.verify(token, config.jwt.secret);
 };
