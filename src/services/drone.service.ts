e0ffc20e9bb39d0eba846ac2bf03a24dcf155062
Eslam Amin <eslam.amin.610@gmail.com>
2026-02-05 21:14:07 +0200

    Created a reseveJob function for the drone

--- src/services/drone.service.ts
+++ src/services/drone.service.ts
@@ -1,8 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, DroneStatus, OrderStatus } from "@prisma/client";
 import { UpdateHeartbeatDto } from "../dtos/drone.dto";
 const prisma = new PrismaClient();
 
 class DroneService {
+  // List all drones (for Admin)
+  async findAll() {
+    return prisma.drone.findMany();
+  }
   // Update Location & Battery
   async updateHeartbeat(droneId: number, data: UpdateHeartbeatDto) {
     return prisma.drone.update({
@@ -11,9 +15,25 @@ class DroneService {
     });
   }
 
-  // List all drones (for Admin)
-  async findAll() {
-    return prisma.drone.findMany();
+  // Drone "Grabs" an order
+  async reserveJob(droneId: number) {
+    // Find a PENDING order
+    const order = await prisma.order.findFirst({
+      where: { status: OrderStatus.PENDING },
+      orderBy: { createdAt: "asc" } // earliest orders first
+    });
+
+    if (!order) throw new Error("No pending jobs available");
+
+    // Assign it
+    return prisma.drone.update({
+      where: { id: droneId },
+      data: {
+        status: DroneStatus.DELIVERING,
+        currentOrder: { connect: { id: order.id } }
+      },
+      include: { currentOrder: true }
+    });
   }
 }
 
