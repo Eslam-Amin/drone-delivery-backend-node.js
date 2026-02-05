8553e93d43838956a1b9fac6e6f2f92cb607d953
Eslam Amin <eslam.amin.610@gmail.com>
2026-02-05 20:39:54 +0200

    Used role as enum from the prisma client role

--- src/dtos/auth.dto.ts
+++ src/dtos/auth.dto.ts
@@ -1,8 +1,9 @@
 import { z } from "zod";
+import { Role } from "@prisma/client";
 
 export const AuthTokenSchema = z.object({
   name: z.string().min(1),
-  role: z.enum(["ADMIN", "ENDUSER", "DRONE"])
+  role: z.enum(Role)
 });
 
 export type AuthTokenDto = z.infer<typeof AuthTokenSchema>;
