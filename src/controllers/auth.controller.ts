be126d52cfee6c09102da46aa55dbfe88cb944b4
Eslam Amin <eslam.amin.610@gmail.com>
2026-02-05 23:40:37 +0200

    Updated Auth Controller to have the validator inside the routes folder

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,16 +1,9 @@
 import { Request, Response } from "express";
 import { AuthService } from "../services/auth.service";
-import { AuthTokenSchema } from "../dtos/auth.dto";
 
 const authService = new AuthService();
 
 export const generateToken = (req: Request, res: Response) => {
-  const validation = AuthTokenSchema.safeParse(req.body);
-
-  if (!validation.success) {
-    return res.status(400).json({ error: validation.error });
-  }
-
-  const token = authService.generateToken(validation.data);
+  const token = authService.generateToken(req.body);
   res.json(token);
 };
