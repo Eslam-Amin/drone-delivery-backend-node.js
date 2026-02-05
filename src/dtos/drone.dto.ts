400488ffc71ca84e86ff59db43dfcf58ece4f9eb
Eslam Amin <eslam.amin.610@gmail.com>
2026-02-05 20:55:27 +0200

    Included battery in the heart beat schema

--- src/dtos/drone.dto.ts
+++ src/dtos/drone.dto.ts
@@ -2,7 +2,8 @@ import { z } from "zod";
 
 export const UpdateHeartbeatSchema = z.object({
   lat: z.number().min(-90).max(90),
-  lng: z.number().min(-180).max(180)
+  lng: z.number().min(-180).max(180),
+  battery: z.number().min(0).max(100)
 });
 
 export type UpdateHeartbeatDto = z.infer<typeof UpdateHeartbeatSchema>;
