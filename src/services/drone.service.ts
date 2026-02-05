3ec6993bb25c5326a569e397e34ca588f532abfe
Eslam Amin <eslam.amin.610@gmail.com>
2026-02-05 21:08:49 +0200

    Created Update drone heart beat service function and used update heartbeat dto

--- src/services/drone.service.ts
+++ src/services/drone.service.ts
@@ -1,7 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+import { UpdateHeartbeatDto } from "../dtos/drone.dto";
 const prisma = new PrismaClient();
 
 class DroneService {
+  // Update Location & Battery
+  async updateHeartbeat(droneId: number, data: UpdateHeartbeatDto) {
+    return prisma.drone.update({
+      where: { id: droneId },
+      data
+    });
+  }
+
   // List all drones (for Admin)
   async findAll() {
     return prisma.drone.findMany();
