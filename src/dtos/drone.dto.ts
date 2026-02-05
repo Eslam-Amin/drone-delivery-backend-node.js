ca06d2091215ac7473aeeb212c282408e916e5f9
Eslam Amin <eslam.amin.610@gmail.com>
2026-02-05 20:56:11 +0200

    Created a validation for update drone status schema

--- src/dtos/drone.dto.ts
+++ src/dtos/drone.dto.ts
@@ -1,9 +1,14 @@
 import { z } from "zod";
+import { DroneStatus } from "@prisma/client";
 
 export const UpdateHeartbeatSchema = z.object({
   lat: z.number().min(-90).max(90),
   lng: z.number().min(-180).max(180),
   battery: z.number().min(0).max(100)
+});
+
+export const UpdateDroneStatusSchema = z.object({
+  status: z.enum(DroneStatus)
 });
 
 export type UpdateHeartbeatDto = z.infer<typeof UpdateHeartbeatSchema>;
