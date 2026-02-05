419f28afa64b290aa5f2043e76c3439a75851652
Eslam Amin <eslam.amin.610@gmail.com>
2026-02-05 20:47:09 +0200

    Created Basic auth service with generate token function

---
+++ src/services/auth.service.ts
@@ -0,0 +1,10 @@
+import { signToken } from "../utils/jwt.utils";
+import { AuthTokenDto } from "../dtos/auth.dto";
+
+export class AuthService {
+  generateToken(data: AuthTokenDto) {
+    // In a real app, we would validate credentials against the DB here.
+    // For this assessment, we sign whatever valid name/role is passed .
+    return { token: signToken(data) };
+  }
+}
