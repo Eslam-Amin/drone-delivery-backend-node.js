0d3e705303c2b0e9a7273b807b2e785ad56c5f8a
Eslam Amin <eslam.amin.610@gmail.com>
2026-02-06 00:28:37 +0200

    Created app routes

---
+++ src/routes/app.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import authRoutes from "./auth.routes";
+import droneRoutes from "./drone.routes";
+import orderRoutes from "./order.routes";
+
+const appRoutes = Router();
+
+// Routes
+appRoutes.use("/auth", authRoutes);
+appRoutes.use("/drones", droneRoutes);
+appRoutes.use("/orders", orderRoutes);
+
+// Health Check
+appRoutes.get("/", (req, res) => {
+  res.json({ status: "Penny Software Drone Backend is Active" });
+});
+
+export default appRoutes;
