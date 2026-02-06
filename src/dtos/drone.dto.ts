aef833d958ef565d5f6d2cb09d4a1e5b2352eb2d
Eslam Amin <eslam.amin.610@gmail.com>
2026-02-06 20:54:09 +0200

    Created a dto for create a drone

--- src/dtos/drone.dto.ts
+++ src/dtos/drone.dto.ts
@@ -1,6 +1,12 @@
 import { z } from "zod";
 import { DroneStatus } from "@prisma/client";
 
+export const CreateDroneSchema = z.object({
+  battery: z.number().min(0).max(100),
+  lat: z.number().min(-90).max(90),
+  lng: z.number().min(-180).max(180)
+});
+
 export const UpdateHeartbeatSchema = z.object({
   lat: z.number().min(-90).max(90),
   lng: z.number().min(-180).max(180),
@@ -11,5 +17,6 @@ export const UpdateDroneStatusSchema = z.object({
   status: z.enum(DroneStatus)
 });
 
+export type CreateDroneDto = z.infer<typeof CreateDroneSchema>;
 export type UpdateHeartbeatDto = z.infer<typeof UpdateHeartbeatSchema>;
 export type UpdateDroneStatusDto = z.infer<typeof UpdateDroneStatusSchema>;
