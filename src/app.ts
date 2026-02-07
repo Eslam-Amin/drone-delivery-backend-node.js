b743a223b0109b03f18a34e6b47d09c46ce356d5
Eslam Amin <eslam.amin.610@gmail.com>
2026-02-07 16:20:13 +0200

    Added the global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import appRouter from "./routes/app.routes";
+import { globalErrorHandler } from "./middlewares/error.middleware";
 const app = express();
 
 app.use(cors());
@@ -8,4 +9,5 @@ app.use(express.json());
 
 app.use("/api/v1", appRouter);
 
+app.use(globalErrorHandler);
 export default app;
