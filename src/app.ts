846dc9e15fcce101e3703f2fe2f5cc66f05ec38c
Eslam Amin <eslam.amin.610@gmail.com>
2026-02-07 16:25:47 +0200

    Installed helmet for more security headers

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 import appRouter from "./routes/app.routes";
 import { globalErrorHandler } from "./middlewares/error.middleware";
-const app = express();
 
+const app = express();
+// Middlewares
+
+// Security headers
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
